959c72eb8e39f367e12b63700c883accef674528
WloMac <wlodzimierz.maciaszczyk@gmail.com>
2023-01-17 19:02:27 +0000

    adding variables

---
+++ assets/js/logic.js
@@ -0,0 +1,6 @@
+const startBtn = document.querySelector("#start");
+const startScreen = document.querySelector("#start-screen");
+const questionSpot = document.querySelector("#questions")
+const title = document.querySelector("#question-title")
+const choices = document.querySelector("#choices")
+const endScreen = document.querySelector("#end-screen")
