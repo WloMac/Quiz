f0a8f994422f309dc7c48e260a307c98f1fb5f08
WloMac <wlodzimierz.maciaszczyk@gmail.com>
2023-01-17 21:47:12 +0000

    adding more questions and timer

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -8,7 +8,7 @@ const timeCounter = document.querySelector("#time")
 
 // Timer
 let timer = 120;
-
+let intervalId;
 
 
 
@@ -16,25 +16,39 @@ let timer = 120;
 var questions = [
     {
         question: "What color is the sky?",
-        choices: ["A. Blue", "B. Green", "C. Brown", "D. Red"],
+        choices: ["A. Blue", "B. Green", "C. Yellow", "D. Red"],
         correct: "A. Blue"
     },
     {
         question: "What color is grass?",
-        choices: ["A. Blue", "B. Green", "C. Brown", "D. Red"],
+        choices: ["A. Blue", "B. Green", "C. Yellow", "D. Red"],
         correct: "B. Green"
+    },
+    {
+        question: "What color is sun?",
+        choices: ["A. Blue", "B. Green", "C. Yellow", "D. Red"],
+        correct: "C. Yellow"
+    },
+    {
+        question: "What color is poppy?",
+        choices: ["A. Blue", "B. Green", "C. Yellow", "D. Red"],
+        correct: "D. Red"
     }
 ]
 
 let index = 0;
 
+console.log(intervalId);
+
+
+
 function start(event) {
     event.preventDefault()
     // Timer
-    setInterval(function() {
+    intervalId = setInterval(function() {
         timer--;
         timeCounter.textContent = timer;   
-    }, 1000)
+    }, 1000) 
 
     choices.innerHTML = ""
     startScreen.setAttribute("class", "hide")
@@ -75,6 +89,7 @@ function start(event) {
 }
 
 function endQuiz() {
+    clearInterval(intervalId);
     startScreen.setAttribute("class", "hide")
     questionSpot.setAttribute("class", "hide")
     endScreen.setAttribute("class", "show")
