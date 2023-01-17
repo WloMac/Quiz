ed28096ecaf0076869afbbe7341d6986a77fce64
WloMac <wlodzimierz.maciaszczyk@gmail.com>
2023-01-17 19:01:12 +0000

    upload of assets

---
+++ assets/js/questions.js
@@ -0,0 +1,18 @@
+var questions = [
+    {
+        question: "Quiz question1",
+        choices: ["A. Answer1", "B. Answer2", "C. Answer3", "D. Answer4"],
+        correct: "A. Answer1"
+    },
+    {
+        question: "Quiz question2",
+        choices: ["A. Answer1", "B. Answer2", "C. Answer3", "D. Answer4"],
+        correct: "B. Answer2"
+    }
+]
+
+
+
+
+
+
